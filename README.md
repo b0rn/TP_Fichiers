06b899e0ddac7afd647a3ab34923a29afea238f9
Victor Leveneur <victor.leveneur@gmail.com>
2019-11-29 14:07:51 +0100

    Updated  README

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
   * int ecrire(void *p, unsigned int taille, unsigned int nbelem, Fichier *f);
   * int fecriref(Fichier *f, char *format, ...);
   * int fliref(Fichier *f, char *format, ...);
+
+# Architecture
+  * bin/ : dossier contenant les exécutables; Les exécutables *.static ont été compilés avec la bibliothèque statique, les exécutables *.dynamic ont été compilés avec la bibliothèque dynamique, les autres ont été compilés sans bibliothèque
+  * include/ : dossier contenant les headers
+  * src/ : dossier contenant les sources
+  * obj/ : dossier contenant les fichiers .o
+  * lib/ : dossier contenant les bibliothèques générées
+
+# Tests réalisés
+  ## Programme main
+    * Ce programme prend 2 fichiers en arguments et copie l'intégralité du 1er fichier dans le 2eme fichier. Ensuite il tente de lire 4 caractères après la fin du 1er fichier, juste pour vérifier que ce n'est pas possible.
+    * Utilisation : ***main fichier1 fichier2***
+  ## Programme formattedString
--- src/main.c
+++ src/main.c
@@ -3,39 +3,6 @@
 #include "fichier.h"
 
 int main (int argc, char **argv){
-  // struct Fichier *f1;
-  // struct Fichier *f2;
-  // char t[2048] = {0};
-  //
-  // if (argc != 3){
-  //   printf("Error \n");
-  //   exit(EXIT_FAILURE);
-  // }
-  //
-  // printf("Ouverture de %s en écriture\n",argv[1]);
-  // f1 = ouvrir(argv[1], 'E');
-  // if (f1 == NULL) {
-  //   printf("Error in First File opening \n");
-  //   exit(EXIT_FAILURE);
-  // }
-  //
-  // printf("Ouverture de %s en lecture\n",argv[2]);
-  // f2 = ouvrir(argv[2], 'L');
-  // if (f2 == NULL) {
-  //   printf("Error in Second File opening \n");
-  //   exit(EXIT_FAILURE);
-  // }
-  //
-  // printf("Lecture de %d caractères :\n",lire(&t,1,300,f2));
-  // printf("%s\n", t);
-  // printf("Lecture de %d caractères :\n",lire(&t,1,91600,f2));
-  // printf("%s\n",t);
-  //
-  // printf("Écriture de %d caractères : \n",ecrire(&t,1,300,f1));
-  //
-  // int i = fermer(f1)+fermer(f2);
-  // printf("Fermer : %d \n", i);
-
   Fichier *f1,*f2;
   char c;
   if(argc != 3) return EXIT_FAILURE;
